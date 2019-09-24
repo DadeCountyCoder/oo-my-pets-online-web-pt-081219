cd381ac766b16426ee25de3282c51e6c1dbb26f5
Alfredo Diaz <mail4alfredo@gmail.com>
2019-09-24 21:36:34 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -34,9 +34,9 @@ class Owner
     Dog.all.select{|dog| dog.owner == self}
   end
 
-  # def buy_cat(name)
-  #   Cat.new(name, self)
-  # end
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
 
   # def buy_dog(name)
   #   Dog.new(name, self)
